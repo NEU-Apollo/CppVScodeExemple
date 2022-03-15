2f4f6f3795f0d7a58e9b85c4c4864675b01ca7fa
NEU-YuanShen <91072677+NEU-YuanShen@users.noreply.github.com>
2022-03-15 23:56:55 +0800

    Add files via upload

---
+++ C++VScodeExemple/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    scanf("%d", &a);
+    a = 10;
+    a++;
+    a = 2 * a;
+    a--;
+    a = a / 3;
+    a = a + 10;
+    printf("%d", a);
+    printf("¹þ¹þ¹þ");
+}
